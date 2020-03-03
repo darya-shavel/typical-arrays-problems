14f3118eb9b0f95da0e28a1a5c0e6426b91c484e
Darya Shavel <darya_shavel98@mail.ru>
2020-03-03 17:14:29 +0300

    feat : solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    }else{
+   let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+          min = array[i];
+        }
+    }
+    return min;
 }
-
+}
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length ===0) {
+        return 0;
+    }else{
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+          max = array[i];
+        }
+    }
+    return max;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+       let sum =0;
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        sum += element;
+    }
+    let avg = sum / array.length;
+  return avg; 
+    }
+    
 }
